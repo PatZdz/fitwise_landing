1bb4301c50c298e91e3a764d77465c2215b84148
Pat Zdziabek <pat.zdziabek@gmail.com>
2024-12-21 21:15:34 +0100

    Update Navbar.tsx

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -28,11 +28,32 @@ export default function Navbar({ isDrawerOpen, setIsDrawerOpen }: NavbarProps) {
     };
   }, []);
 
+  useEffect(() => {
+    if (isDrawerOpen) {
+      document.documentElement.style.overflow = 'hidden';
+      document.body.style.overflow = 'hidden';
+      document.body.style.position = 'fixed';
+      document.body.style.width = '100%';
+    } else {
+      document.documentElement.style.overflow = '';
+      document.body.style.overflow = '';
+      document.body.style.position = '';
+      document.body.style.width = '';
+    }
+
+    return () => {
+      document.documentElement.style.overflow = '';
+      document.body.style.overflow = '';
+      document.body.style.position = '';
+      document.body.style.width = '';
+    };
+  }, [isDrawerOpen]);
   return (
     <>
       <header
-        className={`fixed w-full py-4 bg-white shadow-sm transition-transform duration-300 z-50 ${showNavbar || isDrawerOpen ? "translate-y-0" : "-translate-y-full"
-          }`}
+        className={`fixed w-full py-4 bg-white shadow-sm transition-transform duration-300 z-50 ${
+          showNavbar || isDrawerOpen ? "translate-y-0" : "-translate-y-full"
+        }`}
       >
         <nav className="container mx-auto px-4 flex items-center justify-between">
           <div className="flex items-center gap-2">
@@ -48,7 +69,7 @@ export default function Navbar({ isDrawerOpen, setIsDrawerOpen }: NavbarProps) {
           </div>
 
           <button
-            className="xl:hidden z-50 pr-4"
+            className="xl:hidden z-50"
             onClick={() => setIsDrawerOpen(!isDrawerOpen)}
           >
             {isDrawerOpen ? (
